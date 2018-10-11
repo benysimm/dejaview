3c86ff748042318ef12b37b7d22edfbb957b9dc0
RustyMacGyver <36492095+RustyMacGyver@users.noreply.github.com>
2018-10-12 09:57:44 +1100

    Fleshed out the post method for the api

--- gucci/source/gucci-frontend/Controllers/PointsController.cs
+++ gucci/source/gucci-frontend/Controllers/PointsController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using gucci_frontend.Models;
+using System.Data;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -60,9 +61,38 @@ public class PointsController : Controller
 
     // POST api/<controller>
     [HttpPost]
-    public void Post([FromBody]string value)
+    public int Post([FromBody]Point value)
     {
-    }
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = "guccidb-server.database.windows.net";
+            builder.UserID = "obiwan";
+            builder.Password = "Gucci2018";
+            builder.InitialCatalog = "gucci-db";
+
+            using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+            {
+
+                connection.Open();
+                String sql = "INSERT INTO [dbo].[Points] (Latitude, Longitude) VALUES (@Latitude, @Longitude) SET @ID = SCOPE_IDENTITY();";
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    var param1 = new SqlParameter("Latitude", System.Data.SqlDbType.Float);
+                    var param2 = new SqlParameter("Longitude", System.Data.SqlDbType.Float);
+
+                    param1.Value = value.latitude;
+                    param2.Value = value.longitude;
+                    command.Parameters.Add(param1);
+                    command.Parameters.Add(param2);
+                    command.Parameters.Add("@ID", System.Data.SqlDbType.Int, 4).Direction = ParameterDirection.Output;
+                    command.ExecuteNonQuery();
+                    return (int)command.Parameters["@ID"].Value;
+                }
+            }
+
+
+
+        }
 
     // PUT api/<controller>/5
     [HttpPut("{id}")]
