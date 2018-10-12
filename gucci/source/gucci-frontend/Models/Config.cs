6c79c4cedaeea1cfebfccf74d0c1fb6552fbd128
Ben Simmonds <ben@bencode.net>
2018-10-12 13:03:14 +1100

    Ripped out sidebar into a partial view, and renamed a few nav items with Nina.

---
+++ gucci/source/gucci-frontend/Models/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace gucci_frontend.Models
+{
+    public class Config
+    {
+        public string EsriToken { get; set; }
+    }
+}
